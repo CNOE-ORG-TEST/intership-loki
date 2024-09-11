2b1b6e2e8b4adc0b6676dd832a2ad1346ba40296
Roberto Di Stefano <robertodistefano24@gmail.com>
2024-09-11 11:22:51 +0200

    little correction

--- step-containers/control-plane/check/script.sh
+++ step-containers/control-plane/check/script.sh
@@ -26,7 +26,7 @@ ROLE_ARN="arn:aws:iam::${DEPLOY_AWS_ACCOUNT_ID}:role/${ROLE_NAME}"
 
 assignRoleToServiceAccount "${ROLE_ARN}" "${DEPLOY_AWS_REGION}"
 
-checkSunets "${FE_SUBNET_IDS_PARAMETER}" "${BE_SUBNET_IDS_PARAMETER}"
+checkSubnets "${FE_SUBNET_IDS_PARAMETER}" "${BE_SUBNET_IDS_PARAMETER}"
 
 if [ "$(existClusterCF "${CLOUDFORMATION_NAME}" "${DEPLOY_AWS_REGION}")" = "false"]; then
     # if don't exist cluster CF
